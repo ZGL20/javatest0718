2501cdccdc58d55cafb80ebf6fc248bf928e4430
clearlove9 <https://github.com/ZGL20/test>
2021-07-18 20:56:21 +0800

    test

---
+++ firstdemo0718/src/com/xxxx/Secdemo.java
@@ -0,0 +1,12 @@
+package com.xxxx;
+
+/**
+ * @author ：ZGL
+ * @description：TODO
+ * @date ：2021/7/18 20:25
+ */
+public class Secdemo {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
---
+++ firstdemo0718/src/com/xxxx/firstdemotest.java
@@ -0,0 +1,13 @@
+package com.xxxx;
+
+/**
+ * @author ：ZGL
+ * @description：TODO
+ * @date ：2021/7/18 20:17
+ */
+public class firstdemotest {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+
+    }
+}
